ea21eb8cd0ffc29f7d599b4041a3f4afdfa95d46
Amber Fechko <amber@dendriticspine.com>
2017-06-23 19:18:25 -0700

    Initial upload; main python scripts.

---
+++ wanianki_kanji.py
@@ -0,0 +1,113 @@
+# Uses:
+#   genanki: https://github.com/kerrickstaley/genanki
+#   kanji stroke order font: http://www.nihilist.org.uk/
+#   Pillow: https://python-pillow.org/
+
+import random
+import genanki
+import json
+from PIL import ImageFont
+from PIL import ImageDraw
+from PIL import Image
+
+
+# If you plan to use this script multiple times to update decks, hardcode the model IDs below -- they're unique
+# IDs for your model and deck respectively.
+kanji_model_id = random.randrange(1 << 30, 1 << 31)
+wanianki_kanji_id = random.randrange(1 << 30, 1 << 31)
+
+kanji_model = genanki.Model(kanji_model_id, 'Kanji Model',
+    fields=[
+        {'name': 'Kanji'},
+        {'name': 'Meaning'},
+        {'name': 'Onyomi'},
+        {'name': 'Kunyomi'},
+        {'name': 'Nanori'},
+        {'name': 'Reading'},
+        {'name': 'Level'},
+        {'name': 'KanjiID'},
+    ],
+    templates=[
+        {
+            'name': 'WaniAnki Kanji',
+            'qfmt': '<div class="style-level">Level {{Level}}</div>'
+                    '<div class="style-front">{{Kanji}}</div>',
+            'afmt': '<div class="style-level">Level {{Level}}</div>'
+                    '<img src="{{KanjiID}}.png">'
+                    '<div class="line-dash"></div>'
+                    '<p class="style-meaning">{{Meaning}}</p>'
+                    '<p class="style-reading">{{Reading}}</p>'
+                    '<p class="style-others">Onyomi: {{Onyomi}}</p>'
+                    '<p class="style-others">Kunyomi: {{Kunyomi}}</p>'
+                    '<p class="style-others">Nanori: {{Nanori}}</p>',
+        },
+    ],
+    css=".card { font-family: Noto Sans CJK JP Regular; text-align:center; font-size: 100px; color: #ffffff; background: #f100a1; margin: 45px auto; }"
+        ".style-front { font-family: Noto Sans CJK JP Regular; text-align:center; font-size: 100px; color: #ffffff; background: #f100a1; margin: 50px auto; }"
+        ".style-meaning { font-family: Noto Sans CJK JP Regular; text-align:center; font-size: 60px; color: #ffffff; background: #f100a1; margin: 0px; }"
+        ".style-level { font-size: 10px; font-weight: bold; position: absolute; top: 0; right: 0; margin: 15px; }"
+        ".style-reading { font-family: Noto Sans CJK JP Regular; text-align:center; font-size: 60px; color: #ffffff; background: #f100a1; margin-top: 15px; margin-bottom: 25px; } "
+        ".style-others { font-family: Noto Sans CJK JP Regular; font-size: 30px; color: #ffffff; text-align: center; margin: 0px; padding: 0px; } "
+        ".line-dash { margin: 50px; border-top: #ffffff dashed 2px; }")
+
+wanianki_kanji = genanki.Deck(wanianki_kanji_id, 'WaniAnki Kanji')
+
+kanji_character = []
+kanji_onyomi = []
+kanji_kunyomi = []
+kanji_nanori = []
+kanji_reading = []
+kanji_level = []
+
+kid = 1
+
+with open('wanikani_kanji.json') as kanji:
+    font = ImageFont.truetype("KanjiStrokeOrders_v4.001.ttf", 150)
+    k = json.load(kanji)
+    sortlevel = [x for x in k['requested_information'] if 'level' in x]
+    kanji_by_level = sorted(sortlevel, key=lambda x: x['level'])
+
+    for kan in kanji_by_level:
+        kanji_character = kan['character']
+        img = Image.new("RGB", (200, 200), (241, 0, 161))
+        draw = ImageDraw.Draw(img)
+        draw.text((20, 0), kanji_character, (255, 255, 255), font=font)
+        draw = ImageDraw.Draw(img)
+        img.save(str(kid) + ".png")
+
+        kanji_meaning = kan['meaning']
+        if kan['onyomi']:
+            kanji_onyomi = kan['onyomi']
+        else:
+            kanji_onyomi = "None"
+        if kan['nanori']:
+            kanji_nanori = kan['nanori']
+        else:
+            kanji_nanori = "None"
+        if kan['kunyomi']:
+            kanji_kunyomi = kan['kunyomi']
+        else:
+            kanji_kunyomi = "None"
+
+        kanji_reading = kan['important_reading']
+        kanji_level = str(kan['level'])
+        save_kid = str(kid)
+        if kanji_reading == 'onyomi':
+            new_note = genanki.Note(model=kanji_model, fields=[kanji_character, kanji_meaning, kanji_onyomi, kanji_kunyomi, kanji_nanori, kanji_onyomi, kanji_level, save_kid])
+            wanianki_kanji.add_note(new_note)
+        elif kanji_reading == 'kunyomi':
+            new_note = genanki.Note(model=kanji_model, fields=[kanji_character, kanji_meaning, kanji_onyomi, kanji_kunyomi, kanji_nanori, kanji_kunyomi, kanji_level, save_kid])
+            wanianki_kanji.add_note(new_note)
+        elif kanji_reading == 'nanori':
+            new_note = genanki.Note(model=kanji_model, fields=[kanji_character, kanji_meaning, kanji_onyomi, kanji_kunyomi, kanji_nanori, kanji_kunyomi, kanji_level, save_kid])
+            wanianki_kanji.add_note(new_note)
+        kid += 1
+
+kanji_media_files = []
+kid_range = range(1, kid)
+for i in kid_range:
+    kanji_media_files.append(str(i) + '.png')
+
+wanikani_kanji_package = genanki.Package(wanianki_kanji)
+wanikani_kanji_package.media_files = kanji_media_files
+wanikani_kanji_package.write_to_file('wanikani_kanji.apkg')
---
+++ wanianki_vocab.py
@@ -0,0 +1,84 @@
+# Uses:
+#   genanki: https://github.com/kerrickstaley/genanki
+#   kanji stroke order font: http://www.nihilist.org.uk/
+#   Pillow: https://python-pillow.org/
+
+import random
+import genanki
+import json
+from PIL import ImageFont
+from PIL import ImageDraw
+from PIL import Image
+
+# If you plan to use this script multiple times to update decks, hardcode the model IDs below -- they're unique
+# IDs for your model and deck respectively.
+vocabulary_model_id = random.randrange(1 << 30, 1 << 31)
+wanianki_vocabulary_id = random.randrange(1 << 30, 1 << 31)
+
+vocabulary_model = genanki.Model(vocabulary_model_id, 'Vocabulary Model',
+    fields=[
+        {'name': 'Vocabulary'},
+        {'name': 'Kana'},
+        {'name': 'Meaning'},
+        {'name': 'Level'},
+        {'name': 'VocabID'},
+    ],
+    templates=[
+        {
+            'name': 'WaniAnki Vocabulary',
+            'qfmt': '<div class="style-level">Level {{Level}}</div>'
+                    '<div class="style-front">{{Vocabulary}}</div>',
+            'afmt': '<div class="style-level">Level {{Level}}</div>'
+                    '<img src="v{{VocabID}}.png">'
+                    '<div class="line-dash"></div>'
+                    '<p class="style-reading">{{Kana}}</p>'
+                    '<p class="style-meaning">{{Meaning}}</p>'
+        },
+    ],
+    css=".card { font-family: Noto Sans CJK JP Regular; text-align:center; font-size: 80px; color: #ffffff; background: #a100f1; margin: 45px auto; }"
+        ".style-front { font-family: Noto Sans CJK JP Regular; text-align:center; font-size: 80px; color: #ffffff; background: #a100f1; margin: 50px auto; }"
+        ".style-meaning { font-family: Noto Sans CJK JP Regular; text-align:center; font-size: 50px; color: #ffffff; background: #a100f1; margin: 0px; }"
+        ".style-level { font-size: 10px; font-weight: bold; position: absolute; top: 0; right: 0; margin: 15px; }"
+        ".style-reading { font-family: Noto Sans CJK JP Regular; text-align:center; font-size: 60px; color: #ffffff; background: #a100f1; margin-top: 15px; margin-bottom: 25px; } "
+        ".line-dash { margin: 50px; border-top: #ffffff dashed 2px; }")
+
+wanianki_vocabulary = genanki.Deck(wanianki_vocabulary_id, 'WaniAnki Vocabulary')
+
+vocabulary = []
+vocabulary_kana = []
+vocabulary_meaning = []
+vocabulary_level = []
+
+vid = 1
+maxlength = 0
+
+with open('wanikani_vocabulary.json') as vocab:
+    font = ImageFont.truetype("KanjiStrokeOrders_v4.001.ttf", 100)
+    v = json.load(vocab)
+    sortlevel = [x for x in v['requested_information'] if 'level' in x]
+    vocab_by_level = sorted(sortlevel, key=lambda x: x['level'])
+
+    for voc in vocab_by_level:
+        vocabulary = voc['character']
+        charlength = len(vocabulary)
+        width = (charlength * 103) + (50 - (charlength * 5))
+        img = Image.new("RGB", (width, 140), (161, 0, 241))
+        draw = ImageDraw.Draw(img)
+        draw.text((20, 0), vocabulary, (255, 255, 255), font=font)
+        draw = ImageDraw.Draw(img)
+        img.save('v' + str(vid) + '.png')
+        vocabulary_kana = voc['kana']
+        vocabulary_meaning = voc['meaning']
+        vocabulary_level = str(voc['level'])
+        save_vid = str(vid)
+        new_note = genanki.Note(model=vocabulary_model, fields=[vocabulary, vocabulary_kana, vocabulary_meaning, vocabulary_level, save_vid])
+        wanianki_vocabulary.add_note(new_note)
+        vid += 1
+
+vocab_media_files = []
+vid_range = range(1, vid)
+for i in vid_range:
+    vocab_media_files.append('v' + str(i) + '.png')
+wanianki_vocabulary_package = genanki.Package(wanianki_vocabulary)
+wanianki_vocabulary_package.media_files = vocab_media_files
+wanianki_vocabulary_package.write_to_file('wanikani_vocabulary.apkg')
